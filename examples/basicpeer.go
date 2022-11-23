38112f644751713200b226684a80dd66547fa31e
alvin-reyes <me@areyes.onl>
2022-11-22 21:24:28 -0500

    chore: add basic peer sample

---
+++ examples/basicpeer.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	whypfs "whypfs-core"
+
+	"github.com/ipfs/go-cid"
+)
+
+// Creating a new whypfs node, bootstrapping it with the default bootstrap peers, adding a file to the whypfs network, and
+// then retrieving the file from the whypfs network.
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	whypfsPeer, err := whypfs.NewNode(whypfs.NewNodeParams{
+		Ctx:       ctx,
+		Datastore: whypfs.NewInMemoryDatastore(),
+	})
+	whypfsPeer.BootstrapPeers(whypfs.DefaultBootstrapPeers())
+
+	file, err := whypfsPeer.AddPinFile(context.Background(), bytes.NewReader([]byte("letsrebuildtolearnnewthings!")), nil)
+	if err != nil {
+		return
+	}
+
+	fmt.Println("File CID: ", file.Cid().String())
+
+	c, _ := cid.Decode("bafybeiawc5enlmxtwdbnts3mragh5eyhl3wn5qekvimw72igdj45lixbo4")
+	rsc, err := whypfsPeer.GetFile(ctx, c)
+	if err != nil {
+		panic(err)
+	}
+	defer rsc.Close()
+	content, err := io.ReadAll(rsc)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(content))
+}
--- utils.go
+++ utils.go
@@ -1,24 +1,13 @@
 package whypfs
 
 import (
-	"context"
 	"time"
 
 	datastore "github.com/ipfs/go-datastore"
 	dssync "github.com/ipfs/go-datastore/sync"
-	ipns "github.com/ipfs/go-ipns"
 	libp2p "github.com/libp2p/go-libp2p"
-	dht "github.com/libp2p/go-libp2p-kad-dht"
-	dualdht "github.com/libp2p/go-libp2p-kad-dht/dual"
-	record "github.com/libp2p/go-libp2p-record"
-	"github.com/libp2p/go-libp2p/core/crypto"
-	"github.com/libp2p/go-libp2p/core/host"
 	"github.com/libp2p/go-libp2p/core/peer"
-	"github.com/libp2p/go-libp2p/core/pnet"
-	"github.com/libp2p/go-libp2p/core/routing"
 	"github.com/libp2p/go-libp2p/p2p/net/connmgr"
-	"github.com/libp2p/go-libp2p/p2p/transport/tcp"
-	"github.com/libp2p/go-libp2p/p2p/transport/websocket"
 	"github.com/multiformats/go-multiaddr"
 )
 
@@ -61,76 +50,4 @@ var Libp2pOptionsExtra = []libp2p.Option{
 	libp2p.ConnectionManager(connMgr),
 	libp2p.EnableAutoRelay(),
 	libp2p.EnableNATService(),
-}
-
-// SetupLibp2p returns a routed Host and DHT instances that can be used to
-// easily create a ipfslite Peer. You may consider to use Peer.Bootstrap()
-// after creating the IPFS-Lite Peer to connect to other peers. When the
-// Datastore parameter is nil, the DHT will use an in-memory Datastore, so all
-// provider records are lost on program shutdown.
-//
-// Additional libp2p options can be passed. Note that the Identity,
-// ListenAddrs and PrivateNetwork options will be setup automatically.
-// Interesting options to pass: NATPortMap() EnableAutoRelay(),
-// libp2p.EnableNATService(), DisableRelay(), ConnectionManager(...)... see
-// https://godoc.org/github.com/libp2p/go-libp2p#Option for more info.
-//
-// The secret should be a 32-byte pre-shared-key byte slice.
-func SetupLibp2p(
-	ctx context.Context,
-	hostKey crypto.PrivKey,
-	secret pnet.PSK,
-	listenAddrs []multiaddr.Multiaddr,
-	ds datastore.Batching,
-	opts ...libp2p.Option,
-) (host.Host, *dualdht.DHT, error) {
-
-	var ddht *dualdht.DHT
-	var err error
-	var transports = libp2p.DefaultTransports
-
-	if secret != nil {
-		transports = libp2p.ChainOptions(
-			libp2p.NoTransports,
-			libp2p.Transport(tcp.NewTCPTransport),
-			libp2p.Transport(websocket.New),
-		)
-	}
-
-	finalOpts := []libp2p.Option{
-		libp2p.Identity(hostKey),
-		libp2p.ListenAddrs(listenAddrs...),
-		libp2p.PrivateNetwork(secret),
-		transports,
-		libp2p.Routing(func(h host.Host) (routing.PeerRouting, error) {
-			ddht, err = newDHT(ctx, h, ds)
-			return ddht, err
-		}),
-	}
-	finalOpts = append(finalOpts, opts...)
-
-	h, err := libp2p.New(
-		finalOpts...,
-	)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	return h, ddht, nil
-}
-
-// It creates a new DHT with the given host and datastore, and returns it
-func newDHT(ctx context.Context, h host.Host, ds datastore.Batching) (*dualdht.DHT, error) {
-	dhtOpts := []dualdht.Option{
-		dualdht.DHTOption(dht.NamespacedValidator("pk", record.PublicKeyValidator{})),
-		dualdht.DHTOption(dht.NamespacedValidator("ipns", ipns.Validator{KeyBook: h.Peerstore()})),
-		dualdht.DHTOption(dht.Concurrency(10)),
-		dualdht.DHTOption(dht.Mode(dht.ModeAuto)),
-	}
-	if ds != nil {
-		dhtOpts = append(dhtOpts, dualdht.DHTOption(dht.Datastore(ds)))
-	}
-
-	return dualdht.New(ctx, h, dhtOpts...)
-
 }
