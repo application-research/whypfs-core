87fa01a54ba0fb59fb5ccc2c6bb5cd142a9cad7a
alvin-reyes <me@areyes.onl>
2023-05-14 21:09:08 -0400

    add better car generation example for whypfs-core

--- examples/addcartopeer.go
+++ examples/addcartopeer.go
@@ -6,50 +6,135 @@ import (
 	"fmt"
 	whypfs "github.com/application-research/whypfs-core"
 	"github.com/ipfs/go-cid"
+	chunker "github.com/ipfs/go-ipfs-chunker"
 	format "github.com/ipfs/go-ipld-format"
 	"github.com/ipfs/go-merkledag"
+	"github.com/ipfs/go-unixfs/importer/balanced"
+	"github.com/ipfs/go-unixfs/importer/helpers"
 	"github.com/ipld/go-car"
+	"github.com/multiformats/go-multihash"
 	"io/ioutil"
+	"net/http"
 )
+
+func GetPublicIP() (string, error) {
+	resp, err := http.Get("https://ifconfig.me") // important to get the public ip if possible.
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
+
+func GetCidBuilderDefault() cid.Builder {
+	cidBuilder, err := merkledag.PrefixForCidVersion(1)
+	if err != nil {
+		panic(err)
+	}
+	cidBuilder.MhType = uint64(multihash.SHA2_256)
+	cidBuilder.MhLength = -1
+	return cidBuilder
+}
+func CreateNodeRaw(data []byte, whypfsPeer whypfs.Node) format.Node {
+
+	//rootNode := &merkledag.ProtoNode{}
+	cidBuilder, err := merkledag.PrefixForCidVersion(1)
+	cidBuilder.MhType = uint64(multihash.SHA2_256)
+	cidBuilder.MhLength = -1
+
+	dbp := helpers.DagBuilderParams{
+		Dagserv:    whypfsPeer.DAGService,
+		RawLeaves:  true,
+		Maxlinks:   helpers.DefaultLinksPerBlock,
+		NoCopy:     false,
+		CidBuilder: &cidBuilder,
+	}
+	//
+	spl := chunker.NewSizeSplitter(bytes.NewReader(data), 1024*1024)
+	dbh, err := dbp.New(spl)
+	if err != nil {
+		panic(err)
+	}
+	node1Raw1, err := balanced.Layout(dbh)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return node1Raw1
+}
 
 // Creating a new whypfs node, bootstrapping it with the default bootstrap peers, adding a file to the whypfs network, and
 // then retrieving the file from the whypfs network.
-func AddCarToPeerV1() {
+func AddCarToPeer() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	whypfsPeer, err := whypfs.NewNode(whypfs.NewNodeParams{
-		Ctx:       ctx,
+	publicIp, err := GetPublicIP()
+	newConfig := &whypfs.Config{
+		ListenAddrs: []string{
+			"/ip4/0.0.0.0/tcp/6745",
+			"/ip4/" + publicIp + "/tcp/6745",
+		},
+		AnnounceAddrs: []string{
+			"/ip4/0.0.0.0/tcp/6745",
+			"/ip4/" + publicIp + "/tcp/6745",
+		},
+	}
+	params := whypfs.NewNodeParams{
+		Ctx:       context.Background(),
 		Datastore: whypfs.NewInMemoryDatastore(),
-	})
+		Repo:      ".whypfs1",
+	}
+
+	// node
+	params.Config = params.ConfigurationBuilder(newConfig)
+	whypfsPeer, err := whypfs.NewNode(params)
 	whypfsPeer.BootstrapPeers(whypfs.DefaultBootstrapPeers())
 
-	baseNode := merkledag.NewRawNode([]byte("letsrebuildtolearnnewthings!"))
+	//node1Raw := merkledag.NewRawNode([]byte("letsrebuildtolearnnewthings!1"))
+	node1Raw := CreateNodeRaw([]byte("letsrebuildtolearnnewthings!1"), *whypfsPeer)
+	node2Raw := CreateNodeRaw([]byte("letsrebuildtolearnnewthings!2"), *whypfsPeer)
+	node3Raw := CreateNodeRaw([]byte("letsrebuildtolearnnewthings!3"), *whypfsPeer)
+	node4Raw := CreateNodeRaw([]byte("letsrebuildtolearnnewthings!4"), *whypfsPeer)
+
 	node1 := &merkledag.ProtoNode{}
+	node1.AddNodeLink("node1", node1Raw)
+	node1.SetCidBuilder(GetCidBuilderDefault())
+
 	node2 := &merkledag.ProtoNode{}
+	node2.AddNodeLink("first", node1)
+	node2.AddNodeLink("node2", node2Raw)
+	node2.SetCidBuilder(GetCidBuilderDefault())
+
 	node3 := &merkledag.ProtoNode{}
+	node3.AddNodeLink("node3", node2)
+	node3.AddNodeLink("node31", node3Raw)
+	node3.SetCidBuilder(GetCidBuilderDefault())
+
 	node4 := &merkledag.ProtoNode{}
-	rootNode := &merkledag.ProtoNode{}
-	node1.AddNodeLink("node1 - yehey", baseNode)
-	node1.SetData([]byte("node1 - yehey"))
-	node2.AddNodeLink("node2  - nice", node1)
-	node2.SetData([]byte("node2  - nice"))
-	node3.AddNodeLink("node3 - wow", node2)
-	node3.SetData([]byte("node3 - wow"))
-	node4.AddNodeLink("node4 - cool", node3)
+	//node4.AddNodeLink("node4 - wow node1", node1)
+	//node4.AddNodeLink("node4 - wow node2", node2)
+	node4.AddNodeLink("node43", node3)
 
 	// file
 	file, err := ioutil.ReadFile("test_file_for_car1.log")
+	node4.AddNodeLink("node4root", node4Raw)
 	node4.SetData(file)
+	node4.SetCidBuilder(GetCidBuilderDefault())
 
-	rootNode.AddNodeLink("root - alright", node4)
-	rootNode.SetData([]byte("root - alright"))
-	go fmt.Println("Root CID before: ", rootNode.Cid().String())
-
-	assertAddNodes(whypfsPeer.DAGService, rootNode, node4, node3, node2, node1, baseNode)
+	//rootNode.AddNodeLink("root - alright", node4)
+	//rootNode.SetCidBuilder(cidBuilder)
+	//rootNode.SetData([]byte("root - alright"))
+	//fmt.Println("Root CID before: ", rootNode.Cid().String())
+	assertAddNodes(*whypfsPeer, node1Raw, node2Raw, node3Raw, node4Raw, node1, node2, node3, node4)
 
 	buf := new(bytes.Buffer)
-	if err := car.WriteCar(context.Background(), whypfsPeer.DAGService, []cid.Cid{rootNode.Cid()}, buf); err != nil {
+	if err := car.WriteCar(context.Background(), whypfsPeer.DAGService, []cid.Cid{node4.Cid()}, buf); err != nil {
 		panic(err)
 	}
 	fmt.Println("CAR file size: ", buf.Len())
@@ -58,23 +143,44 @@ func AddCarToPeerV1() {
 		panic(err)
 	}
 
-	fmt.Print(rootNode.Cid().String())
+	fmt.Println("Root CID LoadedCAR: ", ch.Roots[0].String())
+	rootCidToWrite, err := whypfsPeer.Get(ctx, ch.Roots[0])
+	// write raw to data file
+	raw := rootCidToWrite.RawData()
+	err = ioutil.WriteFile("data_file_for_car1.car", raw, 0644)
+
 	for _, c := range ch.Roots {
 		rootCid, err := whypfsPeer.Get(ctx, c)
 		fmt.Println("Root CID after: ", rootCid.String())
+
 		if err != nil {
 			panic(err)
 		}
 		traverseLinks(ctx, whypfsPeer.DAGService, rootCid)
 	}
-}
 
-func assertAddNodes(ds format.DAGService, nds ...format.Node) {
-	for _, nd := range nds {
-		fmt.Println("Adding node: ", nd.Cid().String())
-		if err := ds.Add(context.Background(), nd); err != nil {
+	for _, nd := range []format.Node{node1Raw, node2Raw, node3Raw, node4Raw, node1, node2, node3, node4} {
+		has, err := whypfsPeer.Blockstore.Has(context.TODO(), nd.Cid())
+		if err != nil {
 			panic(err)
 		}
+
+		if !has {
+			fmt.Println("Node not found in blockstore: ", nd.Cid().String())
+			panic("Node not found in blockstore")
+		}
+		fmt.Println("Node found in blockstore: ", nd.Cid().String())
+	}
+}
+
+func assertAddNodes(ds whypfs.Node, nds ...format.Node) {
+	for _, nd := range nds {
+		fmt.Println("Adding node: ", nd.Cid().String())
+		err := ds.DAGService.Add(context.Background(), nd)
+		if err != nil {
+			fmt.Println("Error adding node: ", err)
+		}
+		//fmt.Println("Added node: ", addedNode.Cid().String())
 	}
 }
 
--- examples/main.go
+++ examples/main.go
@@ -4,5 +4,4 @@ import "fmt"
 
 func main() {
 	fmt.Println("Testing Grounds Only!")
-
 }
